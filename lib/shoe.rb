25bed6587f1bebcce8bbe808f69cc46c181f45ea
DANIELLE ALBA <danib.sgh@gmail.com>
2019-03-17 19:51:43 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,25 @@
+require 'pry'
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-
+  BRANDS = []
   def initialize(brand)
     @brand = brand
+    
+    # I had to look this up, I would not have gotten the unless thing on my own. good to put to memory
+    
+     BRANDS << brand unless BRANDS.include?(brand)
+    
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+
+  
+  
+   binding.pry
 
 end
