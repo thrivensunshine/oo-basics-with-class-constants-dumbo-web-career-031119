9d80293252a91ece7ae49f969e033bee801693b6
DANIELLE ALBA <danib.sgh@gmail.com>
2019-03-17 19:43:19 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,10 +3,10 @@ require 'pry'
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-  BRANDS = []
+  @@brands = []
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
+    @@brands << brand
     BRANDS.uniq
   end
 
