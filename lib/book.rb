96cc5dceacdc66358dcd46c17884f01c833dd311
DANIELLE ALBA <danib.sgh@gmail.com>
2019-03-17 19:30:46 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,4 @@
-require'pry'
+
 class Book
   
   attr_accessor :author, :page_count 
@@ -19,5 +19,5 @@ GENRES = []
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-binding.pry
+
 end
